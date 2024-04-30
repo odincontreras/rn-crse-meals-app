56ab8833f553414c99bc460289acec32c9cd8c3f
Odin Contreras <ocontreras@ks2.com>
2024-04-30 16:20:39 -0400

    first commit

---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,72 @@
+import { useLayoutEffect } from "react";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import IconButton from "../components/IconButton";
+import List from "../components/MealDetail/List";
+import Subtitle from "../components/MealDetail/Subtitle";
+import MealDetails from "../components/MealDetails";
+import { MEALS } from "../data/dummy-data";
+
+const MealDetailScreen = ({ route, navigation }) => {
+  const id = route.params.mealId;
+  const selectedMeal = MEALS.find((meal) => meal.id === id);
+
+  function pressHandler() {
+    console.log("pressed");
+  } 
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return <IconButton icon="star" color="white" onPress={pressHandler} />;
+      },
+    });
+  }, [navigation, pressHandler]);
+
+  return (
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
+      />
+
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={selectedMeal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={selectedMeal.steps} />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 250,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 24,
+    margin: 8,
+    color: "white",
+  },
+  detailText: {
+    color: "white",
+  },
+  listOuterContainer: {
+    alignItems: "center",
+    paddingBottom: 32,
+  },
+  listContainer: {
+    width: "80%",
+  },
+});
+
+export default MealDetailScreen;
